e0b6db4f524dc019ea3502cd40451370410c1cfa
Alex Roan <alex.roan@hotmail.com>
2023-10-13 10:42:12 +0100

    Added simlation checks on performUpkeep

--- src/DepositAutomation.sol
+++ src/DepositAutomation.sol
@@ -123,6 +123,7 @@ contract DepositAutomation is ILogAutomation, StreamsLookupCompatibleInterface, 
     ///     - bytes extraData <- This is where the key and addresses array are stored
     /// @dev Decode the performData and call executeDeposit
     function performUpkeep(bytes calldata performData) external onlyForwarder {
+        if (tx.origin == address(0)) return;
         (bytes[] memory values, bytes memory extraData) = abi.decode(performData, (bytes[], bytes));
         (bytes32 key, address[] memory addresses) = abi.decode(extraData, (bytes32, address[]));
         OracleUtils.SetPricesParams memory oracleParams;
